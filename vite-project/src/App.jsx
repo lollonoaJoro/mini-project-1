2cad3ee2e45d79262d76f8f341ac3d9ba127b3b9
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2025-11-10 13:08:26 +0900

    feat: 00기능구현

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,44 +2,35 @@ import { useState, useEffect } from "react";
 import MovieCard from "./details/MovieCard";
 import "./style/App.scss";
 import React from "react";
-import debounce from "./Debounce.jsx";
+import { Router } from "react-router-dom";
+//import debounce from "./Debounce.jsx";
 
 export default function App() {
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-  const APPLICATION_KEY = import.meta.env.VITE_TMDB_API_KEY;
-  console.log("API_KEY:", API_KEY);
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  const [movies, setMovies] = useState([]);
+  console.log(movies);
+  //const [search, setSearch] = useState("");
 
-  <input
-  type="검색하세요"
-  value={movie}
-  onChange={(event) => setmovies(event.target.value)}
-  />;
-  fetch(
-    "https://api.themoviedb.org/3/movie/popular?language=ko&page=1",
-    options
-  )
-
-  .then((res) => res.json())
-  .then((data) => {
-    const resdata = data.results.filter(
-    (tmdbmoive) => tmdbmoive.adult === false
-      );
-      setmovies(resdata);
-    })
-    .catch((err) => console.error(err));
-  }, [];
-
-  return (
-          useEffect(() => {
-    const options = {
+  useEffect(() => {
+    const option = {
       method: "GET",
       headers: {
         accept: "application/json",
         Authorization: `bearer ${API_KEY}`,
-
       },
     };
+
+    fetch(url, option)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  });
+  return (
     <div className="movie-list">
+      <h1>Movie List</h1>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
